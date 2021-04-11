26b426882df71c91c0559a4c654e26062a01e3a2
GhzGarage <qbcore@programmer.net>
2021-04-10 21:56:20 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Weed'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua',
+}
+
+server_scripts {
+    'config.lua',
+    'server/main.lua',
+}
