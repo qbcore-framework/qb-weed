b906e3e472b7352242fd3fd932c0119ce23405ae
GhzGarage <gigahertzgarage@mail.com>
2021-09-11 21:26:25 -0500

    ⚠️⚠️ ghmatti ditch ⚠️⚠️

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,7 @@
 QBCore.Functions.CreateCallback('qb-weed:server:getBuildingPlants', function(source, cb, building)
     local buildingPlants = {}
 
-    exports['ghmattimysql']:execute('SELECT * FROM house_plants WHERE building = @building', {['@building'] = building}, function(plants)
+    exports.oxmysql:fetch('SELECT * FROM house_plants WHERE building = @building', {['@building'] = building}, function(plants)
         for i = 1, #plants, 1 do
             table.insert(buildingPlants, plants[i])
         end
@@ -19,7 +19,7 @@ AddEventHandler('qb-weed:server:placePlant', function(coords, sort, currentHouse
     local random = math.random(1, 2)
     local gender
     if random == 1 then gender = "man" else gender = "woman" end
-    exports.ghmattimysql:execute('INSERT INTO house_plants (building, coords, gender, sort, plantid) VALUES (@building, @coords, @gender, @sort, @plantid)', {
+    exports.oxmysql:insert('INSERT INTO house_plants (building, coords, gender, sort, plantid) VALUES (@building, @coords, @gender, @sort, @plantid)', {
         ['@building'] = currentHouse,
         ['@coords'] = coords,
         ['@gender'] = gender,
@@ -31,27 +31,27 @@ end)
 
 RegisterServerEvent('qb-weed:server:removeDeathPlant')
 AddEventHandler('qb-weed:server:removeDeathPlant', function(building, plantId)
-    exports.ghmattimysql:execute('DELETE FROM house_plants WHERE plantid=@plantid AND building=@building', {['@plantid'] = plantId, ['@building'] = building})
+    exports.oxmysql:execute('DELETE FROM house_plants WHERE plantid=@plantid AND building=@building', {['@plantid'] = plantId, ['@building'] = building})
     TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, building)
 end)
 
 Citizen.CreateThread(function()
     while true do
-        local housePlants = exports.ghmattimysql:executeSync('SELECT * FROM house_plants')
+        local housePlants = exports.oxmysql:fetchSync('SELECT * FROM house_plants', {})
         for k, v in pairs(housePlants) do
             if housePlants[k].food >= 50 then
-                exports.ghmattimysql:execute('UPDATE house_plants SET food=@food WHERE plantid=@plantid', {['@food'] = (housePlants[k].food - 1), ['@plantid'] = housePlants[k].plantid})
+                exports.oxmysql:execute('UPDATE house_plants SET food=@food WHERE plantid=@plantid', {['@food'] = (housePlants[k].food - 1), ['@plantid'] = housePlants[k].plantid})
                 if housePlants[k].health + 1 < 100 then
-                    exports.ghmattimysql:execute('UPDATE house_plants SET health=@health WHERE plantid=@plantid', {['@health'] = (housePlants[k].health + 1), ['@plantid'] = housePlants[k].plantid})
+                    exports.oxmysql:execute('UPDATE house_plants SET health=@health WHERE plantid=@plantid', {['@health'] = (housePlants[k].health + 1), ['@plantid'] = housePlants[k].plantid})
                 end
             end
 
             if housePlants[k].food < 50 then
                 if housePlants[k].food - 1 >= 0 then
-                    exports.ghmattimysql:execute('UPDATE house_plants SET food=@food WHERE plantid=@plantid', {['@food'] = (housePlants[k].food - 1), ['@plantid'] = housePlants[k].plantid})
+                    exports.oxmysql:execute('UPDATE house_plants SET food=@food WHERE plantid=@plantid', {['@food'] = (housePlants[k].food - 1), ['@plantid'] = housePlants[k].plantid})
                 end
                 if housePlants[k].health - 1 >= 0 then
-                    exports.ghmattimysql:execute('UPDATE house_plants SET health=@health WHERE plantid=@plantid', {['@health'] = (housePlants[k].health - 1), ['@plantid'] = housePlants[k].plantid})
+                    exports.oxmysql:execute('UPDATE house_plants SET health=@health WHERE plantid=@plantid', {['@health'] = (housePlants[k].health - 1), ['@plantid'] = housePlants[k].plantid})
                 end
             end
         end
@@ -62,28 +62,28 @@ end)
 
 Citizen.CreateThread(function()
     while true do
-        local housePlants = exports.ghmattimysql:executeSync('SELECT * FROM house_plants')
+        local housePlants = exports.oxmysql:fetchSync('SELECT * FROM house_plants', {})
         for k, v in pairs(housePlants) do
             if housePlants[k].health > 50 then
                 local Grow = math.random(1, 3)
                 if housePlants[k].progress + Grow < 100 then
-                    exports.ghmattimysql:execute('UPDATE house_plants SET progress=@progress WHERE plantid=@plantid', {['@progress'] = (housePlants[k].progress + Grow), ['@plantid'] = housePlants[k].plantid})
+                    exports.oxmysql:execute('UPDATE house_plants SET progress=@progress WHERE plantid=@plantid', {['@progress'] = (housePlants[k].progress + Grow), ['@plantid'] = housePlants[k].plantid})
                 elseif housePlants[k].progress + Grow >= 100 then
                     if housePlants[k].stage ~= QBWeed.Plants[housePlants[k].sort]["highestStage"] then
                         if housePlants[k].stage == "stage-a" then
-                            exports.ghmattimysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-b', ['@plantid'] = housePlants[k].plantid})
+                            exports.oxmysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-b', ['@plantid'] = housePlants[k].plantid})
                         elseif housePlants[k].stage == "stage-b" then
-                            exports.ghmattimysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-c', ['@plantid'] = housePlants[k].plantid})
+                            exports.oxmysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-c', ['@plantid'] = housePlants[k].plantid})
                         elseif housePlants[k].stage == "stage-c" then
-                            exports.ghmattimysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-d', ['@plantid'] = housePlants[k].plantid})
+                            exports.oxmysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-d', ['@plantid'] = housePlants[k].plantid})
                         elseif housePlants[k].stage == "stage-d" then
-                            exports.ghmattimysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-e', ['@plantid'] = housePlants[k].plantid})
+                            exports.oxmysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-e', ['@plantid'] = housePlants[k].plantid})
                         elseif housePlants[k].stage == "stage-e" then
-                            exports.ghmattimysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-f', ['@plantid'] = housePlants[k].plantid})
+                            exports.oxmysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-f', ['@plantid'] = housePlants[k].plantid})
                         elseif housePlants[k].stage == "stage-f" then
-                            exports.ghmattimysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-g', ['@plantid'] = housePlants[k].plantid})
+                            exports.oxmysql:execute('UPDATE house_plants SET stage=@stage WHERE plantid=@plantid', {['@stage'] = 'stage-g', ['@plantid'] = housePlants[k].plantid})
                         end
-                        exports.ghmattimysql:execute('UPDATE house_plants SET progress=@progress WHERE plantid=@plantid', {['@progress'] = 0, ['@plantid'] = housePlants[k].plantid})
+                        exports.oxmysql:execute('UPDATE house_plants SET progress=@progress WHERE plantid=@plantid', {['@progress'] = 0, ['@plantid'] = housePlants[k].plantid})
                     end
                 end
             end
@@ -144,12 +144,12 @@ AddEventHandler('qb-weed:server:harvestPlant', function(house, amount, plantName
     if weedBag ~= nil then
         if weedBag.amount >= sndAmount then
             if house ~= nil then
-                local result = exports.ghmattimysql:executeSync('SELECT * FROM house_plants WHERE plantid=@plantid AND building=@building', {['@plantid'] = plantId, ['@building'] = house}) 
+                local result = exports.oxmysql:fetchSync('SELECT * FROM house_plants WHERE plantid=@plantid AND building=@building', {['@plantid'] = plantId, ['@building'] = house}) 
                 if result[1] ~= nil then
                     Player.Functions.AddItem('weed_'..plantName..'_seed', amount)
                     Player.Functions.AddItem('weed_'..plantName, sndAmount)
                     Player.Functions.RemoveItem('empty_weed_bag', 1)
-                    exports.ghmattimysql:execute('DELETE FROM house_plants WHERE plantid=@plantid AND building=@building', {['@plantid'] = plantId, ['@building'] = house})
+                    exports.oxmysql:execute('DELETE FROM house_plants WHERE plantid=@plantid AND building=@building', {['@plantid'] = plantId, ['@building'] = house})
                     TriggerClientEvent('QBCore:Notify', src, 'The plant has been harvested', 'success', 3500)
                     TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, house)
                 else
@@ -170,16 +170,16 @@ RegisterServerEvent('qb-weed:server:foodPlant')
 AddEventHandler('qb-weed:server:foodPlant', function(house, amount, plantName, plantId)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local plantStats = exports.ghmattimysql:executeSync('SELECT * FROM house_plants WHERE building=@building AND sort=@sort AND plantid=@plantid', {['@building'] = house, ['@sort'] = plantName,['@plantid'] = tostring(plantId)})
+    local plantStats = exports.oxmysql:fetchSync('SELECT * FROM house_plants WHERE building=@building AND sort=@sort AND plantid=@plantid', {['@building'] = house, ['@sort'] = plantName,['@plantid'] = tostring(plantId)})
     TriggerClientEvent('QBCore:Notify', src, QBWeed.Plants[plantName]["label"]..' | Nutrition: '..plantStats[1].food..'% + '..amount..'% ('..(plantStats[1].food + amount)..'%)', 'success', 3500)
     if plantStats[1].food + amount > 100 then
-        exports.ghmattimysql:execute('UPDATE house_plants SET food=@food WHERE building=@building AND plantid=@plantid', {
+        exports.oxmysql:execute('UPDATE house_plants SET food=@food WHERE building=@building AND plantid=@plantid', {
             ['@food'] = 100,
             ['@building'] = house,
             ['@plantid'] = plantId
         })
     else
-        exports.ghmattimysql:execute('UPDATE house_plants SET food=@food WHERE building=@building AND plantid=@plantid', {
+        exports.oxmysql:execute('UPDATE house_plants SET food=@food WHERE building=@building AND plantid=@plantid', {
             ['@food'] = (plantStats[1].food + amount),
             ['@building'] = house,
             ['@plantid'] = plantId
