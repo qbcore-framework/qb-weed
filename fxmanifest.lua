6071e8688f0ed36f91f8eb69ef469facf124aa9c
nosecs <95661721+nosecs@users.noreply.github.com>
2022-01-23 00:03:11 +0100

    test

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,12 +5,7 @@ description 'QB-Weed'
 version '1.0.0'
 
 shared_script 'config.lua'
-
 client_script 'client/main.lua'
-
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
-}
+server_script 'server/main.lua'
 
 lua54 'yes'
