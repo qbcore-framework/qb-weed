50014471352549f4677590e4806c3df1b32a0dda
Nuno Vieira <nunovieira692@gmail.com>
2023-07-21 21:20:56 +0100

    Created pt.lua

---
+++ locales/pt.lua
@@ -0,0 +1,28 @@
+local Translations = {
+    error = {
+        process_canceled = "Processo cancelado",
+        plant_has_died = "A planta morreu. Carrega no ~r~ E ~w~ para remover a planta",
+        cant_place_here = "Não podes colocar a planta aqui",
+        not_safe_here = "Não podes colocar a planta aqui, tenta na tua casa",
+        not_need_nutrition = "A planta não precisa de nutrição",
+        this_plant_no_longer_exists = "Esta planta já não existe",
+        house_not_found = "Casa não encontrada",
+        you_dont_have_enough_resealable_bags = "Tu não tens sacos lacráveis suficientes",
+    },
+    text = {
+        sort = 'Organizar:',
+        harvest_plant = 'Carrega no ~g~ E ~w~ para colher a planta',
+        nutrition = "Nutrição:",
+        health = "Saúde:",
+        harvesting_plant = "A colher planta",
+        planting = "A plantar",
+        feeding_plant = "A alimentar planta",
+        the_plant_has_been_harvested = "A planta foi colhida",
+        removing_the_plant = "A remover a planta",
+    },
+}
+
+Lang = Lang or Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
