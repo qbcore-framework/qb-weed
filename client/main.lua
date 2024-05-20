a7ef1dba77ae3cd6ec834bdd9a0ee5442090c4a2
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-05-20 05:32:49 -0500

    Inventory Update

--- client/main.lua
+++ client/main.lua
@@ -6,7 +6,7 @@ local function drawText3Ds(x, y, z, text)
     SetTextFont(4)
     SetTextProportional(1)
     SetTextColour(255, 255, 255, 215)
-    BeginTextCommandDisplayText("STRING")
+    BeginTextCommandDisplayText('STRING')
     SetTextCentre(true)
     AddTextComponentSubstringPlayerName(text)
     SetDrawOrigin(x, y, z, 0)
@@ -21,11 +21,11 @@ local function spawnHousePlants()
     CreateThread(function()
         for k in pairs(housePlants[currentHouse]) do
             local plantData = {
-                ["plantCoords"] = json.decode(housePlants[currentHouse][k].coords),
-                ["plantProp"] = joaat(QBWeed.Plants[housePlants[currentHouse][k].sort].stages[housePlants[currentHouse][k].stage]),
+                ['plantCoords'] = json.decode(housePlants[currentHouse][k].coords),
+                ['plantProp'] = joaat(QBWeed.Plants[housePlants[currentHouse][k].sort].stages[housePlants[currentHouse][k].stage]),
             }
 
-            local plantProp = CreateObject(plantData["plantProp"], plantData["plantCoords"].x, plantData["plantCoords"].y, plantData["plantCoords"].z, false, false, false)
+            local plantProp = CreateObject(plantData['plantProp'], plantData['plantCoords'].x, plantData['plantCoords'].y, plantData['plantCoords'].z, false, false, false)
             while not plantProp do Wait(0) end
             PlaceObjectOnGroundProperly(plantProp)
             Wait(10)
@@ -41,11 +41,11 @@ local function updateHousePlants(leftHouse)
     CreateThread(function()
         for k in pairs(housePlants[currentHouse]) do
             local plantData = {
-                ["plantCoords"] = json.decode(housePlants[currentHouse][k].coords),
+                ['plantCoords'] = json.decode(housePlants[currentHouse][k].coords),
             }
 
-            for _, stage in pairs(QBWeed.Plants[housePlants[currentHouse][k].sort]["stages"]) do
-                local oldPlant = GetClosestObjectOfType(plantData["plantCoords"].x, plantData["plantCoords"].y, plantData["plantCoords"].z, 3.5, GetHashKey(stage), false, false, false)
+            for _, stage in pairs(QBWeed.Plants[housePlants[currentHouse][k].sort]['stages']) do
+                local oldPlant = GetClosestObjectOfType(plantData['plantCoords'].x, plantData['plantCoords'].y, plantData['plantCoords'].z, 3.5, GetHashKey(stage), false, false, false)
                 if oldPlant ~= 0 then
                     DeleteObject(oldPlant)
                 end
@@ -66,21 +66,21 @@ end
 
 local function getPlantData()
     return {
-        ["plantCoords"] = json.decode(housePlants[currentHouse][closestPlant].coords),
-        ["plantStage"] = housePlants[currentHouse][closestPlant].stage,
-        ["plantProp"] = GetHashKey(QBWeed.Plants[housePlants[currentHouse][closestPlant].sort]["stages"][housePlants[currentHouse][closestPlant].stage]),
-        ["plantSort"] = {
-            ["name"] = housePlants[currentHouse][closestPlant].sort,
-            ["label"] = QBWeed.Plants[housePlants[currentHouse][closestPlant].sort]["label"],
+        ['plantCoords'] = json.decode(housePlants[currentHouse][closestPlant].coords),
+        ['plantStage'] = housePlants[currentHouse][closestPlant].stage,
+        ['plantProp'] = GetHashKey(QBWeed.Plants[housePlants[currentHouse][closestPlant].sort]['stages'][housePlants[currentHouse][closestPlant].stage]),
+        ['plantSort'] = {
+            ['name'] = housePlants[currentHouse][closestPlant].sort,
+            ['label'] = QBWeed.Plants[housePlants[currentHouse][closestPlant].sort]['label'],
         },
-        ["plantStats"] = {
-            ["food"] = housePlants[currentHouse][closestPlant].food,
-            ["health"] = housePlants[currentHouse][closestPlant].health,
-            ["progress"] = housePlants[currentHouse][closestPlant].progress,
-            ["stage"] = housePlants[currentHouse][closestPlant].stage,
-            ["highestStage"] = QBWeed.Plants[housePlants[currentHouse][closestPlant].sort]["highestStage"],
-            ["gender"] = (housePlants[currentHouse][closestPlant].gender == "woman") and "F" or "M",
-            ["plantId"] = housePlants[currentHouse][closestPlant].plantid,
+        ['plantStats'] = {
+            ['food'] = housePlants[currentHouse][closestPlant].food,
+            ['health'] = housePlants[currentHouse][closestPlant].health,
+            ['progress'] = housePlants[currentHouse][closestPlant].progress,
+            ['stage'] = housePlants[currentHouse][closestPlant].stage,
+            ['highestStage'] = QBWeed.Plants[housePlants[currentHouse][closestPlant].sort]['highestStage'],
+            ['gender'] = (housePlants[currentHouse][closestPlant].gender == 'woman') and 'F' or 'M',
+            ['plantId'] = housePlants[currentHouse][closestPlant].plantid,
         }
     }
 end
@@ -105,47 +105,47 @@ local function inHouse()
             Wait(0)
             if plantSpawned and closestPlant ~= 0 then
                 local plantData, status = getPlantData(), 0
-                local plantInfoLabel = Lang:t('text.sort')..' ~g~' ..plantData["plantSort"]["label"]..'~w~ ['..plantData["plantStats"]["gender"]..'] | '..Lang:t('text.nutrition')..' ~b~'..plantData["plantStats"]["food"]..'% ~w~ | '..Lang:t('text.health')..' ~b~'..plantData["plantStats"]["health"]..'%'
-                local plantStageLabel = Lang:t('text.stage').. ' ~b~' .. QBWeed.StageLabels[plantData["plantStats"]["stage"]] ..' ~w~' ..Lang:t('text.progress')..' ~b~'..plantData["plantStats"]["progress"]..'% ~w~ / ' ..Lang:t('text.highestStage') .. ' ~b~' .. QBWeed.StageLabels[plantData["plantStats"]["highestStage"]]
+                local plantInfoLabel = Lang:t('text.sort') .. ' ~g~' .. plantData['plantSort']['label'] .. '~w~ [' .. plantData['plantStats']['gender'] .. '] | ' .. Lang:t('text.nutrition') .. ' ~b~' .. plantData['plantStats']['food'] .. '% ~w~ | ' .. Lang:t('text.health') .. ' ~b~' .. plantData['plantStats']['health'] .. '%'
+                local plantStageLabel = Lang:t('text.stage') .. ' ~b~' .. QBWeed.StageLabels[plantData['plantStats']['stage']] .. ' ~w~' .. Lang:t('text.progress') .. ' ~b~' .. plantData['plantStats']['progress'] .. '% ~w~ / ' .. Lang:t('text.highestStage') .. ' ~b~' .. QBWeed.StageLabels[plantData['plantStats']['highestStage']]
                 local plantActionLabel
 
-                if plantData["plantStats"]["health"] > 0 and plantData["plantStage"] == plantData["plantStats"]["highestStage"] then
+                if plantData['plantStats']['health'] > 0 and plantData['plantStage'] == plantData['plantStats']['highestStage'] then
                     status = 1
                     plantStageLabel = nil
                     plantActionLabel = Lang:t('text.harvest_plant')
-                elseif plantData["plantStats"]["health"] == 0 then
+                elseif plantData['plantStats']['health'] == 0 then
                     status = 2
                     plantStageLabel = nil
                     plantActionLabel = Lang:t('error.plant_has_died')
                 end
 
-                if plantInfoLabel then drawText3Ds(plantData["plantCoords"].x, plantData["plantCoords"].y, plantData["plantCoords"].z, plantInfoLabel) end
-                if plantStageLabel and QBWeed.ShowStages then drawText3Ds(plantData["plantCoords"].x, plantData["plantCoords"].y, plantData["plantCoords"].z - 0.1, plantStageLabel) end
-                if plantActionLabel then drawText3Ds(plantData["plantCoords"].x, plantData["plantCoords"].y, plantData["plantCoords"].z + 0.2, plantActionLabel) end
+                if plantInfoLabel then drawText3Ds(plantData['plantCoords'].x, plantData['plantCoords'].y, plantData['plantCoords'].z, plantInfoLabel) end
+                if plantStageLabel and QBWeed.ShowStages then drawText3Ds(plantData['plantCoords'].x, plantData['plantCoords'].y, plantData['plantCoords'].z - 0.1, plantStageLabel) end
+                if plantActionLabel then drawText3Ds(plantData['plantCoords'].x, plantData['plantCoords'].y, plantData['plantCoords'].z + 0.2, plantActionLabel) end
 
                 if status > 0 and IsControlJustPressed(0, 38) then
                     local ped = PlayerPedId()
 
-                    QBCore.Functions.Progressbar("remove_weed_plant", Lang:t((status == 1) and 'text.harvesting_plant' or 'text.removing_the_plant'), 8000, false, true, {
+                    QBCore.Functions.Progressbar('remove_weed_plant', Lang:t((status == 1) and 'text.harvesting_plant' or 'text.removing_the_plant'), 8000, false, true, {
                         disableMovement = true,
                         disableCarMovement = true,
                         disableMouse = false,
                         disableCombat = true,
                     }, {
-                        animDict = "amb@world_human_gardener_plant@male@base",
-                        anim = "base",
+                        animDict = 'amb@world_human_gardener_plant@male@base',
+                        anim = 'base',
                         flags = 16,
                     }, {}, {}, function() -- Done
                         ClearPedTasks(ped)
                         if status == 1 then
-                            local amount = math.random(1, (plantData["plantStats"]["gender"] == "M") and 2 or 6)
-                            TriggerServerEvent('qb-weed:server:harvestPlant', currentHouse, amount, plantData["plantSort"]["name"], plantData["plantStats"]["plantId"])
+                            local amount = math.random(1, (plantData['plantStats']['gender'] == 'M') and 2 or 6)
+                            TriggerServerEvent('qb-weed:server:harvestPlant', currentHouse, amount, plantData['plantSort']['name'], plantData['plantStats']['plantId'])
                         else
-                            TriggerServerEvent('qb-weed:server:removeDeathPlant', currentHouse, plantData["plantStats"]["plantId"])
+                            TriggerServerEvent('qb-weed:server:removeDeathPlant', currentHouse, plantData['plantStats']['plantId'])
                         end
                     end, function() -- Cancel
                         ClearPedTasks(ped)
-                        QBCore.Functions.Notify(Lang:t('error.process_canceled'), "error")
+                        QBCore.Functions.Notify(Lang:t('error.process_canceled'), 'error')
                     end)
                 end
             else
@@ -175,35 +175,41 @@ RegisterNetEvent('qb-weed:client:refreshHousePlants', function(house)
 end)
 
 RegisterNetEvent('qb-weed:client:placePlant', function(type, item)
-    if not currentHouse then QBCore.Functions.Notify(Lang:t('error.not_safe_here'), 'error', 3500) return end
-    if closestPlant ~= 0 then QBCore.Functions.Notify(Lang:t('error.cant_place_here'), 'error', 3500) return end
+    if not currentHouse then
+        QBCore.Functions.Notify(Lang:t('error.not_safe_here'), 'error', 3500)
+        return
+    end
+    if closestPlant ~= 0 then
+        QBCore.Functions.Notify(Lang:t('error.cant_place_here'), 'error', 3500)
+        return
+    end
     local ped = PlayerPedId()
     local plantCoords = GetOffsetFromEntityInWorldCoords(ped, 0, 0.75, 0)
     local plantData = {
-        ["plantCoords"] = {["x"] = plantCoords.x, ["y"] = plantCoords.y, ["z"] = plantCoords.z},
-        ["plantModel"] = QBWeed.Plants[type]["stages"]["stage-a"],
-        ["plantLabel"] = QBWeed.Plants[type]["label"]
+        ['plantCoords'] = { ['x'] = plantCoords.x, ['y'] = plantCoords.y, ['z'] = plantCoords.z },
+        ['plantModel'] = QBWeed.Plants[type]['stages']['stage-a'],
+        ['plantLabel'] = QBWeed.Plants[type]['label']
     }
 
-    LocalPlayer.state:set("inv_busy", true, true)
-    QBCore.Functions.Progressbar("plant_weed_plant", Lang:t('text.planting'), 8000, false, true, {
+    LocalPlayer.state:set('inv_busy', true, true)
+    QBCore.Functions.Progressbar('plant_weed_plant', Lang:t('text.planting'), 8000, false, true, {
         disableMovement = true,
         disableCarMovement = true,
         disableMouse = false,
         disableCombat = true,
     }, {
-        animDict = "amb@world_human_gardener_plant@male@base",
-        anim = "base",
+        animDict = 'amb@world_human_gardener_plant@male@base',
+        anim = 'base',
         flags = 16,
-        LocalPlayer.state:set("inv_busy", false, true)
+        LocalPlayer.state:set('inv_busy', false, true)
     }, {}, {}, function() -- Done
         ClearPedTasks(ped)
-        TriggerServerEvent('qb-weed:server:placePlant', json.encode(plantData["plantCoords"]), type, currentHouse)
+        TriggerServerEvent('qb-weed:server:placePlant', json.encode(plantData['plantCoords']), type, currentHouse)
         TriggerServerEvent('qb-weed:server:removeSeed', item.slot, type)
     end, function() -- Cancel
         ClearPedTasks(ped)
-        QBCore.Functions.Notify(Lang:t('error.process_canceled'), "error")
-        LocalPlayer.state:set("inv_busy", false, true)
+        QBCore.Functions.Notify(Lang:t('error.process_canceled'), 'error')
+        LocalPlayer.state:set('inv_busy', false, true)
     end)
 end)
 
@@ -213,32 +219,32 @@ RegisterNetEvent('qb-weed:client:foodPlant', function()
         local ped = PlayerPedId()
         local plantData = getPlantData()
 
-        if plantData["plantStats"]["food"] == 100 then
+        if plantData['plantStats']['food'] == 100 then
             QBCore.Functions.Notify(Lang:t('error.not_need_nutrition'), 'error', 3500)
         else
-            LocalPlayer.state:set("inv_busy", true, true)
-            QBCore.Functions.Progressbar("plant_weed_plant", Lang:t('text.feeding_plant'), math.random(4000, 8000), false, true, {
+            LocalPlayer.state:set('inv_busy', true, true)
+            QBCore.Functions.Progressbar('plant_weed_plant', Lang:t('text.feeding_plant'), math.random(4000, 8000), false, true, {
                 disableMovement = true,
                 disableCarMovement = true,
                 disableMouse = false,
                 disableCombat = true,
             }, {
-                animDict = "timetable@gardener@filling_can",
-                anim = "gar_ig_5_filling_can",
+                animDict = 'timetable@gardener@filling_can',
+                anim = 'gar_ig_5_filling_can',
                 flags = 16,
 
-                LocalPlayer.state:set("inv_busy", false, true)
+                LocalPlayer.state:set('inv_busy', false, true)
             }, {}, {}, function() -- Done
                 ClearPedTasks(ped)
                 local newFood = math.random(40, 60)
-                TriggerServerEvent('qb-weed:server:foodPlant', currentHouse, newFood, plantData["plantSort"]["name"], plantData["plantStats"]["plantId"])
+                TriggerServerEvent('qb-weed:server:foodPlant', currentHouse, newFood, plantData['plantSort']['name'], plantData['plantStats']['plantId'])
             end, function() -- Cancel
                 ClearPedTasks(ped)
-                LocalPlayer.state:set("inv_busy", false, true)
-                QBCore.Functions.Notify(Lang:t('error.process_canceled'), "error")
+                LocalPlayer.state:set('inv_busy', false, true)
+                QBCore.Functions.Notify(Lang:t('error.process_canceled'), 'error')
             end)
         end
     else
-        QBCore.Functions.Notify(Lang:t('error.cant_place_here'), "error")
+        QBCore.Functions.Notify(Lang:t('error.cant_place_here'), 'error')
     end
 end)
--- server/main.lua
+++ server/main.lua
@@ -3,33 +3,33 @@ local QBCore = exports['qb-core']:GetCoreObject()
 QBCore.Functions.CreateCallback('qb-weed:server:getBuildingPlants', function(_, cb, building)
     local buildingPlants = {}
 
-    MySQL.query('SELECT * FROM house_plants WHERE building = ?', {building}, function(plants)
+    MySQL.query('SELECT * FROM house_plants WHERE building = ?', { building }, function(plants)
         for i = 1, #plants, 1 do
             buildingPlants[#buildingPlants + 1] = plants[i]
         end
-        
+
         cb(buildingPlants)
     end)
 end)
 
 RegisterNetEvent('qb-weed:server:placePlant', function(coords, sort, currentHouse)
     local random = math.random(1, 2)
-    local gender = (random == 1) and "man" or "woman"
+    local gender = (random == 1) and 'man' or 'woman'
 
     MySQL.insert('INSERT INTO house_plants (building, coords, gender, sort, plantid) VALUES (?, ?, ?, ?, ?)',
-        {currentHouse, coords, gender, sort, math.random(111111, 999999)})
+        { currentHouse, coords, gender, sort, math.random(111111, 999999) })
     TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, currentHouse)
 end)
 
 RegisterNetEvent('qb-weed:server:removeDeathPlant', function(building, plantId)
-    MySQL.query('DELETE FROM house_plants WHERE plantid = ? AND building = ?', {plantId, building})
+    MySQL.query('DELETE FROM house_plants WHERE plantid = ? AND building = ?', { plantId, building })
     TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, building)
 end)
 
 RegisterServerEvent('qb-weed:server:removeSeed', function(itemslot, seed)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.RemoveItem(seed, 1, itemslot)
+    exports['qb-inventory']:RemoveItem(src, seed, 1, itemslot, 'qb-weed:server:removeSeed')
 end)
 
 RegisterNetEvent('qb-weed:server:harvestPlant', function(house, amount, plantName, plantId)
@@ -37,17 +37,14 @@ RegisterNetEvent('qb-weed:server:harvestPlant', function(house, amount, plantNam
     local Player = QBCore.Functions.GetPlayer(src)
     local weedBag = Player.Functions.GetItemByName('empty_weed_bag')
     local sndAmount = math.random(12, 16)
-
     if weedBag ~= nil and weedBag.amount >= sndAmount then
         if house ~= nil then
-            local result = MySQL.query.await(
-                'SELECT * FROM house_plants WHERE plantid = ? AND building = ?', {plantId, house})
-
+            local result = MySQL.query.await('SELECT * FROM house_plants WHERE plantid = ? AND building = ?', { plantId, house })
             if result[1] ~= nil then
-                Player.Functions.AddItem('weed_' .. plantName .. '_seed', amount)
-                Player.Functions.AddItem('weed_' .. plantName, sndAmount)
-                Player.Functions.RemoveItem('empty_weed_bag', sndAmount)
-                MySQL.query('DELETE FROM house_plants WHERE plantid = ? AND building = ?', {plantId, house})
+                exports['qb-inventory']:AddItem(src, 'weed_' .. plantName .. '_seed', amount, false, false, 'qb-weed:server:harvestPlant')
+                exports['qb-inventory']:AddItem(src, 'weed_' .. plantName, sndAmount, false, false, 'qb-weed:server:harvestPlant')
+                exports['qb-inventory']:RemoveItem(src, 'empty_weed_bag', sndAmount, false, 'qb-weed:server:harvestPlant')
+                MySQL.query('DELETE FROM house_plants WHERE plantid = ? AND building = ?', { plantId, house })
                 TriggerClientEvent('QBCore:Notify', src, Lang:t('text.the_plant_has_been_harvested'), 'success', 3500)
                 TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, house)
             else
@@ -64,31 +61,22 @@ end)
 RegisterNetEvent('qb-weed:server:foodPlant', function(house, amount, plantName, plantId)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local plantStats = MySQL.query.await(
-        'SELECT * FROM house_plants WHERE building = ? AND sort = ? AND plantid = ?',
-        {house, plantName, tostring(plantId)})
-
+    local plantStats = MySQL.query.await('SELECT * FROM house_plants WHERE building = ? AND sort = ? AND plantid = ?',{ house, plantName, tostring(plantId) })
     local updatedFood = math.min(100, plantStats[1].food + amount)
-    TriggerClientEvent('QBCore:Notify', src, QBWeed.Plants[plantName]["label"] ..
-        ' | Nutrition: ' .. plantStats[1].food .. '% + ' .. updatedFood - plantStats[1].food .. '% (' ..
-        updatedFood .. '%)', 'success', 3500)
-
-    MySQL.update('UPDATE house_plants SET food = ? WHERE building = ? AND plantid = ?',
-        {updatedFood, house, plantId})
-    Player.Functions.RemoveItem('weed_nutrition', 1)
+    TriggerClientEvent('QBCore:Notify', src, QBWeed.Plants[plantName]['label'] ..' | Nutrition: ' .. plantStats[1].food .. '% + ' .. updatedFood - plantStats[1].food .. '% (' ..updatedFood .. '%)', 'success', 3500)
+    MySQL.update('UPDATE house_plants SET food = ? WHERE building = ? AND plantid = ?',{ updatedFood, house, plantId })
+    exports['qb-inventory']:RemoveItem(src, 'weed_nutrition', 1, false, 'qb-weed:server:foodPlant')
     TriggerClientEvent('qb-weed:client:refreshHousePlants', -1, house)
 end)
 
 AddEventHandler('onResourceStart', function(resourceName)
     if resourceName ~= GetCurrentResourceName() then return end
-
     for plantName, _ in pairs(QBWeed.Plants) do
-        QBCore.Functions.CreateUseableItem("weed_" .. plantName .. "_seed", function(source, item)
+        QBCore.Functions.CreateUseableItem('weed_' .. plantName .. '_seed', function(source, item)
             TriggerClientEvent('qb-weed:client:placePlant', source, plantName, item)
         end)
     end
-
-    QBCore.Functions.CreateUseableItem("weed_nutrition", function(source, item)
+    QBCore.Functions.CreateUseableItem('weed_nutrition', function(source, item)
         TriggerClientEvent('qb-weed:client:foodPlant', source, item)
     end)
 end)
@@ -102,11 +90,11 @@ CreateThread(function()
                 local Grow = math.random(QBWeed.Progress.min, QBWeed.Progress.max)
                 if housePlants[k].progress + Grow < 100 then
                     MySQL.update('UPDATE house_plants SET progress = ? WHERE plantid = ?',
-                        {(housePlants[k].progress + Grow), housePlants[k].plantid})
+                        { (housePlants[k].progress + Grow), housePlants[k].plantid })
                 elseif housePlants[k].progress + Grow >= 100 then
-                    if housePlants[k].stage ~= QBWeed.Plants[housePlants[k].sort]["highestStage"] then
+                    if housePlants[k].stage ~= QBWeed.Plants[housePlants[k].sort]['highestStage'] then
                         MySQL.update('UPDATE house_plants SET stage = ?, progress = 0 WHERE plantid = ?',
-                            {housePlants[k].stage + 1, housePlants[k].plantid})
+                            { housePlants[k].stage + 1, housePlants[k].plantid })
                     end
                 end
             end
@@ -115,7 +103,7 @@ CreateThread(function()
                 local plantHealth = (plantFood >= 50) and math.min(100, plant.health + 1) or math.max(0, plant.health - 1)
 
                 MySQL.update('UPDATE house_plants SET food = ?, health = ? WHERE plantid = ?',
-                    {plantFood, plantHealth, plant.plantid})
+                    { plantFood, plantHealth, plant.plantid })
             end
         end
 
